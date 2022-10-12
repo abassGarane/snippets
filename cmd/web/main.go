e8141c643e5631717cc3dacecb95311c7eddbc1f
abassGarane <thelibertinewreck@gmail.com>
2022-10-12 18:02:25 +0300

    Added custom logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main()  {
-  log.Println("Starting main server on port 4000...")
+  //Parsing commandline flags
+  addr := flag.String("addr",":4000","HTTP network address")
+  flag.Parse()
+  
+  // custom logger
+  infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+  errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile|log.Llongfile)
+  infoLog.Printf("Starting main server on port %s...\n",*addr)
+  fileServer := http.FileServer(http.Dir("./ui/static"))
+
   mux := http.NewServeMux()
   mux.HandleFunc("/", Home)
   mux.HandleFunc("/snippet",ShowSnippet)
   mux.HandleFunc("/snippet/create",CreateSnippet)
+  mux.Handle("/static/", http.StripPrefix("/static",fileServer))
   
 
-  if err := http.ListenAndServe(":4000",mux); err != nil{
-    log.Fatal(err)
+  if err := http.ListenAndServe(*addr,mux); err != nil{
+    errLog.Fatal(err)
   }
 }
